ae4310ed7a13c82fefac0da66599ce66f72f52f0
nitin anand <nitinanand27@yahoo.com>
2016-05-23 11:18:19 +0200

    changes in register

--- Wordless/Wordless/Controllers/UserController.cs
+++ Wordless/Wordless/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Wordless.Models;
 
 namespace Wordless.Controllers
 {
@@ -11,51 +12,8 @@ namespace Wordless.Controllers
         // GET: User
         public ActionResult Register()
         {
-            ///Get values from the form entered by the user in /Default/Login.cshtml
-            string tmpUsername = Request["registerUsername"];
-            string tmpEmail = Request["registerEmail"];
-            string tmpPassword = Request["registerPassword"];
-
-            if(tmpEmail.IndexOf || tmpUsername.Length <= 1 || 
-                tmpUsername.Length > 15 || tmpPassword.Length<6)
-            {
-
-                ///Return to login/register -view to try again
-                TempData["error"] = "Please check username, email and password!";
-                return Redirect("/Default/Login");
-            }
-
-            try
-            {
-                ///Connect to dB and create user
-                using (NewsterContext nc = new NewsterContext())
-                {
-                    string passwdEncrypted = HelpClass.Encrypt(tmpPassword);
-
-                    ///Check for existing username
-                    if (nc.Users.Where(x=>x.UserName.ToLower() == tmpUsername.ToLower()).Count() == 0)
-                    {
-                        User tmpUser = new User() { UserName = tmpUsername, Email = tmpEmail, Password = passwdEncrypted ,Confirmed=false };
-                        nc.Users.Add(tmpUser);
-                        nc.SaveChanges();
-                    }
-
-                    else
-                    {
-                        TempData["error"] = "User exists!";
-                        return Redirect("/Default/Login");
-                    }
-                }
-
-                return Redirect("/Default/Index");
-            }
-
-            catch
-            {
-                ///Return to login/register -view to try again
-                TempData["error"] = "Database fail!";
-                return Redirect("/Default/Login");
-            }
+            WordlessContext db = new WordlessContext();
+           
             return View();
         }
 
