4417d39cfab3845c6b78dfbdf012e4e7d61d4219
Patrik Nilsson <patteee@gmail.com>
2016-05-25 14:20:25 +0200

    Lagt till book-controller

---
+++ Wordless/Wordless/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wordless.Models;
+using System.Data.Entity;
+
+namespace Wordless.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            WordlessContext db = new WordlessContext();
+            var bookList = db.Book.Include(b => b.Author).Include(c => c.Comments).ToList();
+            
+            return View(bookList);
+        }
+    }
+}
