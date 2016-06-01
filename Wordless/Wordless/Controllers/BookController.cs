ab504e42869e1b3d8b4f8d3e7e0e5d22a122bbea
ChrilleJ <christian.jarenfors@gmail.com>
2016-06-01 16:18:27 +0200

    scary commit

--- Wordless/Wordless/Controllers/BookController.cs
+++ Wordless/Wordless/Controllers/BookController.cs
@@ -24,6 +24,54 @@ namespace Wordless.Controllers
             var booklist = db.Books.Include(b => b.Author).Include(c => c.Comments).Where(b => b.Genre== (Genres)id).ToList();
             return View("Index", booklist);
         }
+        public ActionResult BooksAPI( string searchstring)
+        {
+            WordlessContext context = new WordlessContext();
+            var booklist1 = context.Books.Where(b => b.Title.ToLower().Contains(searchstring.ToLower()));
+            var booklist2 = context.Books.Where(b => b.Genre.ToString().ToLower().Contains(searchstring.ToLower()));
+            var booklist3 = context.Books.Where(b => b.BookText.ToLower().Contains(searchstring.ToLower()));
+            var booklist4 = context.Books.Where(b => b.Author.Name.ToLower().Contains(searchstring.ToLower()));
+
+            var FinalBookList = booklist1.ToList();
+            foreach(var b in booklist2)
+            {
+                if(!FinalBookList.Any(v=> v == b))
+                {
+                    FinalBookList.Add(b);
+                }
+            }
+            foreach (var b in booklist3)
+            {
+                if (!FinalBookList.Any(v => v == b))
+                {
+                    FinalBookList.Add(b);
+                }
+            }
+            foreach (var b in booklist4)
+            {
+                if (!FinalBookList.Any(v => v == b))
+                {
+                    FinalBookList.Add(b);
+                }
+            }
+            if (searchstring=="")
+            {
+                FinalBookList = context.Books.ToList();
+            }
+            string htmlstring = "";
+            foreach(Book b in FinalBookList)
+            {
+                htmlstring = htmlstring + "<div class='bodyDiv col-md-5 col-lg-5'>"
+                            + "<p class='bookTitle'>"+ b.Title+"<button class='pull-right btn btn-default' type='button' onclick='location.href='@Url.Action('Buy', 'Book')''>Buy</button></p>"
+                            + "<p class='bookAuthor'>Written by: "+ b.Author.Name+"</p>"
+                            + "<p class='bookAuthor'>Genre: "+ b.Genre+"</p>"
+                            + "<p class='booktText'>Price: "+ b.Price+"</p>"
+                            + "<p class='booktText'>"+ b.BookText+" </p>"
+                        + "</div>";
+            }
+            return Json(new { HtmlString = htmlstring },
+                JsonRequestBehavior.AllowGet);
+        }
 
     }
 }
---
+++ Wordless/Wordless/Scripts/ChristianJavascript.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $('#Search').on('input', function () {
+    
+        $.ajax({
+            method:"GET",
+            url: '/Book/BooksAPI',
+            dataType: 'json',
+            data: {
+                
+                searchstring: $("#Search").val()
+            },
+            success: function (data) {
+                $('#BookDisplay').html(data.HtmlString);
+            },
+            error: function (jqXHR, statusText, errorThrown) {
+                $('#BookDisplay').html('Ett fel inträffade: <br>'
+                    + statusText);
+            }
+        });
+    });
+});
